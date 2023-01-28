5d7fc39f4558c2de30426bc191cbf401185c99e8
Aziz1019 <starinc1998@gmail.com>
2023-01-28 16:00:12 +0500

    Initial Commit

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -5,5 +5,15 @@ import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        int counter = 1;
+        for (String s : sourceList) {
+            if (counter % 3 == 0) {
+                arrayList.add(s);
+                arrayList.add(s);
+            }
+            counter++;
+        }
+        return arrayList;
     }
 }
